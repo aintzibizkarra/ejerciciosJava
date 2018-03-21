bda2baf67b494082d6ae9d2ac582228bca3673eb
Curso <Curso@Formacion-TOSH2.ipartekaula.com>
2018-03-21 14:27:43 +0100

    Hamburguesa corregido

--- src/com/ipartek/formacion/capitulo3_1/Hamburguesa.java
+++ src/com/ipartek/formacion/capitulo3_1/Hamburguesa.java
@@ -2,23 +2,39 @@ package com.ipartek.formacion.capitulo3_1;
 
 public class Hamburguesa {
 
-	public static float LISTA_INGREDIENTES_PRECIO[] = { (float) 0.30, (float) 0.70, (float) 0.50, (float) 0.25,
-			(float) 0.30, (float) 0.70 };
-	public static String[] LISTA_INGREDIENTES = { "pan", "carne", "tomate", "lechuga", "pepino", "queso" };
+	public static final String[] LISTA_INGREDIENTES = { "pan", "carne", "tomate", "lechuga", "pepino", "queso" };
+	public static final float[] LISTA_INGREDIENTES_PRECIO = { 0.30f, 0.70f, .50f, .25f, .30f, .70f };
 
+	// atributo
 	private boolean[] ingredientes;
 
 	public Hamburguesa() {
 		super();
-
-		this.ingredientes[0] = true;
-		this.ingredientes[1] = true;
-
+		this.ingredientes = new boolean[LISTA_INGREDIENTES.length];
+		this.ingredientes[0] = true; // pan
+		this.ingredientes[1] = true; // carne
 	}
 
-	public float getPrecioHamburguesa() {
-		return 0;
+	public boolean[] getIngredientes() {
+		return ingredientes;
+	}
 
+	public void setIngredientes(boolean[] ingredientes) {
+		this.ingredientes = ingredientes;
+	}
+
+	public float getPrecio() {
+		float resul = 0;
+
+		for (int i = 0; i < this.ingredientes.length; i++) {
+
+			if (ingredientes[i]) {
+				resul += LISTA_INGREDIENTES_PRECIO[i];
+			}
+
+		}
+
+		return resul;
 	}
 
 }
--- src/com/ipartek/formacion/capitulo3_1/HamburguesaTest.java
+++ src/com/ipartek/formacion/capitulo3_1/HamburguesaTest.java
@@ -1,7 +1,6 @@
 package com.ipartek.formacion.capitulo3_1;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
@@ -9,18 +8,21 @@ public class HamburguesaTest {
 
 	@Test
 	public void testHamburguesa() {
-		fail("Not yet implemented");
+
 	}
 
 	@Test
-	public void testGetPrecioHamburguesa() {
-		Hamburguesa viuda = new Hamburguesa();
-		assertEquals(1.0f, viuda.getPrecioHamburguesa(), 0.01);
+	public void testGetPrecio() {
 
-		Hamburguesa completa = new Hamburguesa();
+		Hamburguesa viuda = new Hamburguesa();
+		assertEquals(1.0f, viuda.getPrecio(), 0.01);
+
 		boolean[] ingredientesCompleta = { true, true, true, true, true, true };
-		// completa.setIngredientes(ingredientesCompleta);
-		assertEquals(2.75f, completa.getPrecioHamburguesa(), 0.01);
+		Hamburguesa completa = new Hamburguesa();
+
+		completa.setIngredientes(ingredientesCompleta);
+		assertEquals(2.75f, completa.getPrecio(), 0.01);
+
 	}
 
 }
